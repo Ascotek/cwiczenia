4b5d95bd93ba36601d09e0d91cebffc6fb150fed
Ascotek <paulinatochwin@gmail.com>
2020-01-04 16:17:32 +0100

    commit2

--- src/main/java/samouczek/Slownik.java
+++ src/main/java/samouczek/Slownik.java
@@ -12,7 +12,7 @@ public class Slownik {
         Scanner odczyt = new Scanner(System.in);
 
         System.out.println("Witaj! Dodawanie wpisu - 1; Sprawdzanie - 2; Usuwanie - 3; Wyjście - 4" +
-                "Którą czynność chcesz wykonać?");
+                " Którą czynność chcesz wykonać?");
 
         int czynnosc;
         System.out.println("Podaj nr czynności: ");
@@ -25,6 +25,8 @@ public class Slownik {
 
         Dictionary<String, String> dictionary = new Hashtable<>();
         dictionary.put("Magda", "Sii");
+        dictionary.put("Anna", "Lego");
+
 
         if(czynnosc>=4 && czynnosc<0) {
             System.out.println("Mozesz podac tylko numer od 1 do 4");
@@ -40,44 +42,30 @@ public class Slownik {
                 System.out.println("Podaj hasło:");
                 hasło = odczyt.next();
 
-                System.out.println("Dodane slowa to: " + dictionary.put(klucz, hasło) + dictionary);
+                dictionary.put(klucz, hasło);
+                System.out.println("W słowniku są: " + dictionary);
+
                 break;
 
             case 2:
                 String klucz1;
                 System.out.println("Podaj klucz:");
                 klucz1 = odczyt.next();
+                boolean spr = ((Hashtable<String, String>) dictionary).containsKey(klucz1);
 
-                for(int i=0; i<dictionary.size(); i++){
-                    if(klucz1.equals(i)){
-                        System.out.println("Już istnieje taki klucz");
-                    }else
-                        System.out.println("Twoj klucz jest OK");
-                }
-
-                String hasło1;
-                System.out.println("Podaj hasło:");
-                hasło1 = odczyt.next();
-
-                for(int i=0; i<dictionary.size(); i++) {
-                    if (hasło1.equals(i)) {
-                        System.out.println("Już istnieje takie hasło");
-                    } else
-                        System.out.println("Twóje hasło jest ok");
-                }
-                dictionary.put(klucz1, hasło1);
-
-                System.out.println("Teraz slownik zawiera: " + dictionary);
+                System.out.println("Czy ten klucz istnieje? " + spr);
                 break;
 
             case 3:
                 System.out.println("Podaj klucz:");
                 klucz = odczyt.next();
 
-                System.out.println("SLownik usunal: " + dictionary.remove(klucz) +  dictionary);
+                String usunietyWyraz = dictionary.remove(klucz);
+
+                System.out.println("Słownik usunał: " + usunietyWyraz  + " W słowniku pozostało: " + dictionary);
                 break;
             case 4:
-                System.out.println("Wyjscie");
+                System.out.println("Wyjście");
                 break;
             default:
                 System.out.println("Koniec");
